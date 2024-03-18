12e853c586ab7844051d1a5a1dfbd3cfb9703c0d
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-18 10:30:51 +0530

    added Box

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "#8e0b00" }}>Box1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
+      <Box style={{ backgroundColor: "#91b543" }}>Box4</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box5</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 64,
+    borderWidth: 6,
+    borderBlockColor: "black",
   },
 });
