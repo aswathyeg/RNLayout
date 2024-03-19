37bb16dcb7a459de75c575b02247422a075b2b24
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-03-19 18:14:21 +0530

    added dimensions

--- App.js
+++ App.js
@@ -1,25 +1,32 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
 import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e0b00" }}>Box1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box2</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box3</Box>
-      <Box style={{ backgroundColor: "#91b543" }}>Box4</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box5</Box>
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome</Text>
+      </View>
     </View>
   );
 }
-
+const widthDimension = Dimensions.get("window").width;
+const heightDimension = Dimensions.get("window").height;
 const styles = StyleSheet.create({
   container: {
-    marginTop: 64,
-    borderWidth: 6,
-    borderBlockColor: "black",
+    backgroundColor: "plum",
     flex: 1,
     justifyContent: "center",
-    gap: 10,
+    alignItems: "center",
+  },
+  box: {
+    backgroundColor: "lightblue",
+    width: widthDimension > 500 ? "70%" : "90%",
+    height: heightDimension > 600 ? "60%" : "90%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
   },
 });
